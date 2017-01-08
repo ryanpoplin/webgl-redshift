4447c5ca1ec85beaf432281f114117ce2dff9280
ryanpoplin <byrdannfox@gmail.com>
2017-01-08 00:16:17 -0500

    make hapi properly serve static content for the webgl poc

--- server.js
+++ server.js
@@ -1,1 +1,48 @@
 'use strict';
+const Hapi = require('hapi');
+// const Path = require('path');
+const server = new Hapi.Server({
+    /*connections: {
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'public')
+            }
+        }
+    }*/
+});
+server.register(require('inert'), () => {console.log('inert');});
+server.connection({port: 8080});
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, reply) => {
+        reply.file('./public/index.html');
+    }
+});
+server.route({
+    method: 'GET',
+    path: '/css',
+    handler: (request, reply) => {
+        reply.file('./public/css/setup.css')
+    }
+});
+server.route({
+    method: 'GET',
+    path: '/babylonjs',
+    handler: (request, reply) => {
+        reply.file('./public/bower_components/babylonjs/dist/babylon.2.5.js')
+    }
+});
+server.route({
+    method: 'GET',
+    path: '/js',
+    handler: (request, reply) => {
+        reply.file('./public/js/app.js')
+    }
+});
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log(server.info.uri);
+});
