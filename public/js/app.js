44e05a99cb38348c1c1031d1113ef5e5370c5e55
ryanpoplin <byrdannfox@gmail.com>
2017-01-10 01:10:24 -0500

    got the GPU optimization code added so we can have some serious fun but i'll need to refactor to get it to work with the data coming from Redshift

--- public/js/app.js
+++ public/js/app.js
@@ -8,14 +8,18 @@ BjsApp.init = function (coordinatesArr) {
     var camera = new BABYLON.FreeCamera('FreeCamera', new BABYLON.Vector3(0, 2, -15), scene);
     camera.attachControl(canvas);
     var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
-    engine.runRenderLoop(function () { scene.render(); });
-    for (let i = 0; i <= coordinatesArr.length - parseInt(995); i++) {
+    // engine.runRenderLoop(function () { scene.render(); });
+    // for (let i = 0; i <= coordinatesArr.length - parseInt(995); i++) {
         var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 1, scene);
-        sphere.position.x = coordinatesArr[i].x;
-        sphere.position.y = coordinatesArr[i].y;
-        sphere.position.z = coordinatesArr[i].z;
+        for (let i = 0; i < 1000; i++) {
+            var mesh = sphere.createInstance();
+            mesh.position.y += i * 2;
+        }
+        sphere.position.x = coordinatesArr.x; // coordinatesArr[0].x;
+        sphere.position.y = coordinatesArr.y; // coordinatesArr[0].y;
+        sphere.position.z = coordinatesArr.z; // coordinatesArr[0].z;
         engine.runRenderLoop(function () {
             scene.render();
         });
-    }
+    // }
 };
